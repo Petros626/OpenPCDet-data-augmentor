c91dfe21b0a6a194ecca6fe002fc26d1f6417fc2
Petros626 <petros.katsoulakos@web.de>
2025-12-27 11:47:25 +0100

    add bbox hint

--- pcdet/utils/object3d_zod.py
+++ pcdet/utils/object3d_zod.py
@@ -1,5 +1,5 @@
+# source: https://github.com/zenseact/EdgeAnnotationZChallenge/blob/35afb0dcffd6b7ca3982a9a3ffbe50e9c92875f0/eval/convert_annotations_to_kitti.py#L25 
 def zod_occlusion_to_kitti(zod_occlusion):
-    # source: https://github.com/zenseact/EdgeAnnotationZChallenge/blob/35afb0dcffd6b7ca3982a9a3ffbe50e9c92875f0/eval/convert_annotations_to_kitti.py#L25 
     mapping = {
         'None': 0,
         'Light': 1,
@@ -12,7 +12,7 @@ def zod_occlusion_to_kitti(zod_occlusion):
     return mapping.get(zod_occlusion, 3)  # 3 = unknown
 
 def get_zod_obj_level(obj):
-    height = float(obj.box2d.ymax) - float(obj.box2d.ymin) + 1
+    height = float(obj.box2d.ymax) - float(obj.box2d.ymin) + 1 # x_min, y_min, x_max, y_max
 
     if height >= 40 and obj.truncation <= 0.15 and obj.occlusion <= 0:
         return 0 # Easy
